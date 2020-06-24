c1ce6dc10bfb34c2b7cb9f062d436b2b1bdb2620
Kelli Conklin <kelli.alexandrea@gmail.com>
2020-06-24 10:17:01 -0600

    Add files via upload

---
+++ basics.py
@@ -0,0 +1,7 @@
+monday_temps = [9.1, 8.8, 7.5]
+student_grades = {"Mary": 9.1, "John": 8.8, "Fred":7.5}
+
+mytotal = sum(student_grades.values())
+length = len(student_grades)
+mean = mytotal / length 
+print(mean)
