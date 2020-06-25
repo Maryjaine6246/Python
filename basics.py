0dddd9422e887e6fed634800b3c84f9e4f0a94af
Maryjaine6246 <kelli.alexandrea@gmail.com>
2020-06-25 10:56:42 -0600

    Added tuples

--- basics.py
+++ basics.py
@@ -10,6 +10,9 @@ print(mynumber, mytext)
 
 #Lists Practice
 
-student_grades = [9.1, 8.8, 7.5]
+student_grades = {"Mary": 9.1, "John": 8.8, "Jeff": 7.5}
 
-
+mysum = sum(student_grades.values())
+length = len(student_grades)
+mean = mysum / length
+print(mean) 
